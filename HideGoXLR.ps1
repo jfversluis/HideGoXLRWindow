382f81aee3e219b0dffe8b3d1108f18841b62c46
Gerald Versluis <github@geraldversluis.nl>
2020-07-10 10:35:43 +0200

    Update HideGoXLR.ps1

--- HideGoXLR.ps1
+++ HideGoXLR.ps1
@@ -27,7 +27,6 @@ while (($attempts -gt 0) -or ($keepAlive -eq $true)) {
 
         # We'll Hide the window through its handle
         $Win32ShowWindowAsync::ShowWindowAsync($_, 0) | Out-Null
-        break;
     }
 
     Write-Output "Attempts remaining: $attempts, sleeping for $sleep, keep alive: $keepAlive"
